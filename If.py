ddb7b0a7a9015f7b2f3b5e4b7bb5fb3b6164db99
santiago-quinga <66694164+santiago-quinga@users.noreply.github.com>
2020-06-23 12:49:10 -0500

    Create If.py
    Ejemplos para el uso de If

---
+++ If.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 23 11:06:57 2020
+
+@author: Santiago Quinga
+"""
+
+nativa=100
+datos=100
+if nativa==datos:
+    print("La VLAN nativa"
+          " es igual a la de datos!!!")
+else:
+    print("La  VLAN es diferente!!!")
