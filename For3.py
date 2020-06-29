450349f047250e926c5a82f131ed8abecbe0b32f
santiago-quinga <66694164+santiago-quinga@users.noreply.github.com>
2020-06-29 12:15:25 -0500

    Practice_For

---
+++ For3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 26 11:19:35 2020
+
+@author: Santiago Quinga
+"""
+
+switch=[]
+router=[]
+lista=["R1","R2","R3",
+       "R4","S1","S2","S3"]
+for i in lista:
+    if "S" in i:
+        switch.append(i)
+    else:
+        router.append(i)
+print(switch)        
+print(router)
