f9871c42f01a49127ee0e18626e429c2e9f66262
santiago-quinga <66694164+santiago-quinga@users.noreply.github.com>
2020-06-29 13:05:18 -0500

    Practice_For

---
+++ For1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 29 13:03:56 2020
+
+@author: Usuario
+"""
+
+lista=["R1","R2","R3","R4",5,2.8,True]
+for a in lista:
+    print(a)
---
+++ For2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 29 13:01:59 2020
+
+@author: Santiago Quinga
+"""
+
+lista=["R1","R2","R3","R4","S1","S2","S3"]
+for i in lista:
+    if "3" in i:
+        print(i)
